3d61c583f34cde807f577b35edc872fdf072e173
Chantal <chantalyang17@gmail.com>
2015-09-20 21:24:35 +0200

    Add checkbox to add/remove layer for fibre

--- map.js
+++ map.js
@@ -9,20 +9,26 @@ function initMap() {
 
   add_fibre_layer(map);
 
-	//Geojson
-	// za = new google.maps.Data();
-	// za.loadGeoJson("za.json");
-	// za.setStyle({strokeWeight: 1, strokeColor:"purple"});
-	// za.setMap(map);
-
-
 
 }
 
 function add_fibre_layer(gmap){
 	fibre_data_layer = new google.maps.Data();
-	fibre_data_layer.loadGeoJson("final.json");
+	fibre_data_layer.loadGeoJson("fibre.json");
 	fibre_data_layer.setStyle({strokeWeight: 1, strokeColor:"purple"});
 	fibre_data_layer.setMap(gmap);  
+
+}
+
+function changeLayer(selected_layer){
+
+	if (document.getElementById("fibre").checked == true){
+			fibre_data_layer.setMap(map);
+	}
+	
+
+	if (document.getElementById("fibre").checked == false){
+		fibre_data_layer.setMap(null);
+	}
 
 }
