43b8f5caa154b46a404059e9383db1d9fb6fbeb8
Chantal <chantalyang17@gmail.com>
2015-09-20 22:58:40 +0200

    Add probe data to map with filter

--- map.js
+++ map.js
@@ -7,29 +7,85 @@ function initMap() {
     streetViewControl: false,
 });
 
-  add_fibre_layer(map);
-  document.getElementById("fibre").checked == true;
+	add_fibre_layer(map);
+	add_probe_layer(map);
 
+	// var myTitle = document.createElement('h1');
+	// myTitle.style.color = 'white';
+	// myTitle.innerHTML = 'Hello World';
+	// var myTextDiv = document.createElement('div');
+	// myTextDiv.appendChild(myTitle);
+
+	// map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(myTextDiv);
 
 }
 
 function add_fibre_layer(gmap){
 	fibre_data_layer = new google.maps.Data();
-	fibre_data_layer.loadGeoJson("fibre.json");
-	fibre_data_layer.setStyle({strokeWeight: 1, strokeColor:"purple"});
+	fibre_data_layer.loadGeoJson("/data/fibre.json");
+	fibre_data_layer.setStyle({strokeWeight: 1, strokeColor:"purple", strokeOpacity:0.5});
 	fibre_data_layer.setMap(gmap);  
 
 }
 
-function changeLayer(selected_layer){
+function add_probe_layer(gmap){
 
-	if (document.getElementById("fibre").checked == true){
-			fibre_data_layer.setMap(map);
+	var probe_symbol = {
+		path: google.maps.SymbolPath.CIRCLE,
+		scale:6,
+		fillColor: 'white',
+		fillOpacity: 1,
+		strokeColor: "red",
+		strokeWeight:2,
+	};
+
+	probe_layer = new google.maps.Data();
+	probe_layer.loadGeoJson("/data/all_probes.json");
+	probe_layer.setMap(gmap);
+	probe_layer.setStyle({icon: probe_symbol, clickable: true});
+
+	var  infoWindow = new google.maps.InfoWindow({
+	content: "",
+	});
+
+	 //Mouseover events listener
+	 var probe_mousover_listener = probe_layer.addListener('mouseover', function(event) {
+
+        //Display infowindow
+        infoWindow.setContent(event.feature.getProperty("name") + 
+        	"<br>" + "<b>Probe ID: </b> " + event.feature.getProperty("probe_id") +
+        	"<br>" + " <b>ASN:</b>" + event.feature.getProperty("asn"));
+        var anchor = new google.maps.MVCObject();
+        anchor.set("position",event.latLng);
+        infoWindow.open(map,anchor);
+      });//End event listener
 	}
-	
 
-	if (document.getElementById("fibre").checked == false){
-		fibre_data_layer.setMap(null);
+	function changeLayer(selected_layer){
+
+		//Fibre
+		if (selected_layer == "fibre"){
+			if (document.getElementById("fibre_layer").checked == true){
+				fibre_data_layer.setMap(map);
+			}
+
+
+			if (document.getElementById("fibre_layer").checked == false){
+				fibre_data_layer.setMap(null);
+			}
+		}
+
+		//Probes
+		if (selected_layer == "probes"){
+			if (document.getElementById("probe_layer").checked == true){
+				probe_layer.setMap(map);
+			}
+
+
+			if (document.getElementById("probe_layer").checked == false){
+				probe_layer.setMap(null);
+			}
+		}
+
+
 	}
-
-}
